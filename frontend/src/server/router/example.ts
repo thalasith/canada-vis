9376444deca25f4a5c8130128a2fa706ebea5f2b
Boris Tsao <boris.tsao@gmail.com>
2022-08-29 22:08:34 -0700

    fixing some errors

--- frontend/src/server/router/example.ts
+++ frontend/src/server/router/example.ts
@@ -1,52 +1,5 @@
 import { createRouter } from "./context";
 import { z } from "zod";
-import { Prisma } from "@prisma/client";
-
-// interface DataType {
-//   male_0_to_4_years: number,
-//   male_5_to_9_years: number,
-//   male_10_to_14_years: number,
-//   male_15_to_19_years: number,
-//   male_20_to_24_years: number,
-//   male_25_to_29_years: number,
-//   male_30_to_34_years: number,
-//   male_35_to_39_years: number,
-//   male_40_to_44_years: number,
-//   male_45_to_49_years: number,
-//   male_50_to_54_years: number,
-//   male_55_to_59_years: number,
-//   male_60_to_64_years: number,
-//   male_65_to_69_years: number,
-//   male_70_to_74_years: number,
-//   male_75_to_79_years: number,
-//   male_80_to_84_years: number,
-//   male_85_to_89_years: number,
-//   male_90_to_94_years: number,
-//   male_95_to_99_years: number,
-//   male_100_years_and_over: number,
-//   female_0_to_4_years: number,
-//   female_5_to_9_years: number,
-//   female_10_to_14_years: number,
-//   female_15_to_19_years: number,
-//   female_20_to_24_years: number,
-//   female_25_to_29_years: number,
-//   female_30_to_34_years: number,
-//   female_35_to_39_years: number,
-//   female_40_to_44_years: number,
-//   female_45_to_49_years: number,
-//   female_50_to_54_years: number,
-//   female_55_to_59_years: number,
-//   female_60_to_64_years: number,
-//   female_65_to_69_years: number,
-//   female_70_to_74_years: number,
-//   female_75_to_79_years: number,
-//   female_80_to_84_years: number,
-//   female_85_to_89_years: number,
-//   female_90_to_94_years: number,
-//   female_95_to_99_years: number,
-//   female_100_years_and_over: number,
-
-// }
 
 export const exampleRouter = createRouter()
   .query("hello", {
