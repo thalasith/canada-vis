2ed6d91c8cae8c437e474fdcb10c0ffdfc2f957a
Boris Tsao <boris.tsao@gmail.com>
2022-08-30 22:14:45 -0700

    trying to push out ssr and other features

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -32,31 +32,10 @@ const TestChart = ({ ageData }: any) => {
           }
         />
         <Legend />
-        <Bar dataKey="male" stackId="a" fill="#8884d8" />
-        <Bar dataKey="female" stackId="a" fill="#82ca9d" />
+        <Bar dataKey="male" stackId="a" fill="#D62618" />
+        <Bar dataKey="female" stackId="a" fill="#FFBABA" />
       </BarChart>
     </div>
-    // <ResponsiveContainer width="100%" height="100%">
-    //   <BarChart
-    //     width={500}
-    //     height={300}
-    //     data={data}
-    //     margin={{
-    //       top: 20,
-    //       right: 30,
-    //       left: 20,
-    //       bottom: 5,
-    //     }}
-    //   >
-    //     <CartesianGrid strokeDasharray="3 3" />
-    //     <XAxis dataKey="name" />
-    //     <YAxis />
-    //     <Tooltip />
-    //     <Legend />
-    //     <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-    //     <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
-    //   </BarChart>
-    // </ResponsiveContainer>
   );
 };
 
