371039d70b1847761e94d639d5e8a91d95c9a8e8
Boris Tsao <boris.tsao@gmail.com>
2022-09-13 19:07:05 -0700

    fixing next bug

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useState } from "react";
 // import { GiHamburgerMenu, GiBasketballBall } from "react-icons/gi";
 
@@ -7,14 +8,16 @@ const Header = () => {
   return (
     <nav className="relative mb-3 flex flex-wrap items-center justify-between bg-red-700 px-2 py-3">
       <div className="container mx-auto flex flex-wrap items-center justify-between px-4">
-        <a
+        <Link
           href="/"
-          className="flex flex-row rounded-full text-2xl font-bold  text-white hover:text-orange-500"
+
           // className=" flex w-full flex-row text-2xl font-bold text-white lg:static lg:block lg:w-auto lg:justify-start"
         >
           {/* <GiBasketballBall className="mt-1 mr-1" color="EA8500" />{" "} */}
-          Canada Census Visualization Tool
-        </a>
+          <p className="flex flex-row rounded-full text-2xl font-bold  text-white ">
+            Canada Census Visualization Tool
+          </p>
+        </Link>
         <button
           className="block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-xl leading-none text-white outline-none focus:outline-none lg:hidden"
           type="button"
@@ -29,7 +32,7 @@ const Header = () => {
             (navbarOpen ? " flex flex-col" : " hidden")
           }
         >
-          <ul className="flex list-none flex-col justify-end px-3 lg:ml-auto lg:flex-row lg:px-0">
+          {/* <ul className="flex list-none flex-col justify-end px-3 lg:ml-auto lg:flex-row lg:px-0">
             <li className="nav-item">
               <div className="mt-2 flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-white lg:mt-0">
                 Hi there
@@ -40,7 +43,7 @@ const Header = () => {
                 There too
               </div>
             </li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </nav>
